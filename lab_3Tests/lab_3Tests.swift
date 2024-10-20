b3430bc291e792086026218cfb2ab6d5f4ff3f31
AshleyCardot <acardot@smu.edu>
2024-10-20 13:54:32 -0500

    Initial Commit

---
+++ lab_3Tests/lab_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  lab_3Tests.swift
+//  lab_3Tests
+//
+//  Created by Ashley Cardot on 10/20/24.
+//
+
+import Testing
+@testable import lab_3
+
+struct lab_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
